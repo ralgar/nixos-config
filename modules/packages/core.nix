e7e8cf5a31aa9b8e3ce8e071e01ec37ee9022209
Ryan Algar <me@ralgar.dev>
2026-04-15 21:01:09 -0700

    feat(firefox): configure basic system-wide defaults

--- modules/packages/core.nix
+++ modules/packages/core.nix
@@ -8,7 +8,6 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     alacritty
-    firefox
     gammastep
     grim
     hypridle
@@ -26,6 +25,35 @@
     yadm
     zsh-powerlevel10k
   ];
+
+  programs.firefox = {
+    enable = true;
+
+    policies = {
+      # Install uBlock Origin
+      ExtensionSettings = {
+        "uBlock0@raymondhill.net" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "force_installed";
+        };
+      };
+
+      # Force the built-in dark color scheme
+      DisplayMenuBar = "default-off";
+      Preferences = {
+        "extensions.activeThemeID" = {
+          Value = "firefox-compact-dark@mozilla.org";
+          Status = "locked";  # Or "default" to allow users to override
+        };
+      };
+    };
+
+    # Native vertical tabs (requires Firefox 135+)
+    preferences = {
+      "sidebar.revamp" = true;
+      "sidebar.verticalTabs" = true;
+    };
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
