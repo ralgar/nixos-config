a75e2920f872c6d9cea36ac2f032d52b1ec2a4e3
Ryan Algar <me@ralgar.dev>
2026-04-16 22:45:11 -0700

    refactor(flake): switch to unstable repo

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
     dotfiles = {
