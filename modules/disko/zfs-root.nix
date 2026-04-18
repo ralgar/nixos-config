9475d1883a72a4bab2b13e7e31a282f8a2dcff2b
Ryan Algar <me@ralgar.dev>
2026-04-18 01:02:48 -0700

    fix(vmWithDisko): workaround for bug where /home is on a volume

--- modules/disko/zfs-root.nix
+++ modules/disko/zfs-root.nix
@@ -114,5 +114,11 @@
         };
       };
     };
+
+    # Workaround for https://github.com/nix-community/disko/issues/1157
+    systemd.tmpfiles.rules = lib.mkIf (config.virtualisation ? qemu) (
+      map (name: "d /home/${name} 0700 ${name} users -")
+        (builtins.attrNames (lib.filterAttrs (_: u: u.isNormalUser) config.users.users))
+    );
   };
 }
