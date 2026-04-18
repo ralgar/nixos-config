1e57612912efff3a18bdd181b7513d637c8ee3ba
Ryan Algar <me@ralgar.dev>
2026-04-17 23:37:56 -0700

    feat(firefox): improve configuration

--- modules/packages/core.nix
+++ modules/packages/core.nix
@@ -31,29 +31,95 @@
   programs.firefox = {
     enable = true;
 
+    preferences = {
+      "browser.newtabpage.activity-stream.showWeather" = false;
+    };
+
     policies = {
-      # Install uBlock Origin
-      ExtensionSettings = {
+      # Feature Disabling
+      DisableFirefoxStudies         = true;
+      DisableFirefoxAccounts        = true;
+      DisableFirefoxScreenshots     = true;
+      DisableForgetButton           = true;
+      DisableMasterPasswordCreation = true;
+      DisableProfileImport          = true;
+      DisableProfileRefresh         = true;
+      DisableSetDesktopBackground   = true;
+      DisablePocket                 = true;
+      DisableTelemetry              = true;
+      DisableFormHistory            = true;
+      DisablePasswordReveal         = true;
+
+      # UI and Behavior
+      DisplayBookmarksToolbar = "never";
+      DisplayMenuBar = "never";
+      OfferToSaveLogins = false;
+
+      DNSOverHTTPS = {
+        Enabled = false;
+        Locked = true;
+      };
+
+      # Extensions
+      ExtensionSettings = let
+        mozilla = short: "https://addons.mozilla.org/firefox/downloads/latest/${short}/latest.xpi";
+      in {
+        "*".installation_mode = "blocked";
+
         "uBlock0@raymondhill.net" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          install_url = mozilla "ublock-origin";
+          installation_mode = "force_installed";
+          private_browsing = true;
+        };
+
+        "addon@darkreader.org" = {
+          install_url = mozilla "darkreader";
+          installation_mode = "force_installed";
+        };
+
+        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+          install_url = mozilla "bitwarden-password-manager";
+          installation_mode = "force_installed";
+        };
+
+        "jid1-Y3WfE7td45aWDw@jetpack" = {
+          install_url = mozilla "behind_the_overlay";
+          installation_mode = "force_installed";
+        };
+
+        "jid1-KKzOGWgsW3Ao4Q@jetpack" = {
+          install_url = mozilla "i-dont-care-about-cookies";
+          installation_mode = "force_installed";
+        };
+
+        "enhancerforyoutube@maximerf.addons.mozilla.org" = {
+          install_url = mozilla "enhancer-for-youtube";
           installation_mode = "force_installed";
         };
       };
 
-      # Force the built-in dark color scheme
-      DisplayMenuBar = "default-off";
-      Preferences = {
-        "extensions.activeThemeID" = {
-          Value = "firefox-compact-dark@mozilla.org";
-          Status = "locked";  # Or "default" to allow users to override
-        };
+      FirefoxHome = {
+        Search = true;
+        TopSites = false;
+        SponsoredTopSites = false;
+        Highlights = false;
+        Pocket = false;
+        Stories = false;
+        SponsoredPocket = false;
+        SponsoredStories = false;
+        Snippets = false;
+        Locked = true;
       };
-    };
 
-    # Native vertical tabs (requires Firefox 135+)
-    preferences = {
-      "sidebar.revamp" = true;
-      "sidebar.verticalTabs" = true;
+      Preferences = {
+        # Force the built-in dark color scheme
+        "extensions.activeThemeID" = { Value = "firefox-compact-dark@mozilla.org"; Status = "locked"; };
+      };
+
+      SearchEngines = {
+        Default = "DuckDuckGo";
+        Remove = ["Google" "Bing" "eBay" "Wikipedia (en)" "Perplexity" ];
+      };
     };
   };
 
