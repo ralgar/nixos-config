2131074fa41b7d94877483dde2ae6c7fb992d1cb
Ryan Algar <me@ralgar.dev>
2026-09-21 20:52:36 -0700

    chore(flake): switch to 26.05 stable branch

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
     dotfiles = {
